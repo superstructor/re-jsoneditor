fb12044c51f8d230ade105be9c0bd3e29545dd08
Isaac Johnston <isaac@superstruct.nz>
2021-04-06 12:30:54 +1200

    Upgrade jsoneditor to 9.3.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## Unreleased
+
+#### Changed
+
+- Upgrade jsoneditor to 9.3.0
+
+## 0.0.1 (2021-03-24)
+
+Initial release.
--- src/deps.cljs
+++ src/deps.cljs
@@ -1,2 +1,2 @@
-{:npm-deps     {"jsoneditor" "9.2.0"}
+{:npm-deps     {"jsoneditor" "9.3.0"}
  :npm-dev-deps {"shadow-cljs" "2.11.24"}}
