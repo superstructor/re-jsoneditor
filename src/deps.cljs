c2fb1e3903d714cad77293f5ef3a6d5df49745de
Isaac Johnston <isaac@superstruct.nz>
2021-07-31 12:48:49 +1200

    Upgrade ClojureScript to 1.10.879 and shadow-cljs to 2.15.2

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,4 @@
 {:npm-deps     {"jsoneditor"  "9.5.0"}
- :npm-dev-deps {"shadow-cljs" "2.14.5"
+ :npm-dev-deps {"shadow-cljs" "2.15.2"
                 "react"       "17.0.2"
                 "react-dom"   "17.0.2"}}
