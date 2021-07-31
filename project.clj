dd3ce1c292ba5c900a2bf53efeb001c5756f3abb
Isaac Johnston <isaac@superstruct.nz>
2021-07-31 12:49:23 +1200

    Upgrade lein-shadow to 0.4.0

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
 
 
   :plugins [[day8/lein-git-inject "0.0.14"]
-            [lein-shadow          "0.3.1"]
+            [lein-shadow          "0.4.0"]
             [lein-shell           "0.5.0"]]
 
   :middleware [leiningen.git-inject/middleware]
